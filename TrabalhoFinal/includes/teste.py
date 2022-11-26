6b96441c2f7180d198ac004895dfcc04e19f55fe
arturgcr <arturgcr.20221@poli.ufrj.br>
2022-11-26 00:27:38 -0300

    atualização e teste de exibição do menu
    talvez criar um objeto produto seja interessante para implementar botões no menu atrelados o produto

--- TrabalhoFinal/includes/teste.py
+++ TrabalhoFinal/includes/teste.py
@@ -1,12 +1,57 @@
-import PySimpleGUI as sg
+# importa a biblioteca que cria a aba do app
+from tkinter import *
+from csv import *
+import pandas as pd
 
-layout=[[sg.Text("qual o prolema dessa merda?")]]
+ # define a aba como a chamada da criação da biblioteca
+menu = Tk()
 
-janela = sg.Window("titulo", layout)
+# define o nome da aba criada
+menu.title("Kilivros")
 
-while True:
-    evento, valores = janela.read()
-    if evento == sg.WINDOW.CLOSED:
-        break
+#tamanho da aba do app (todas as medidas são em pixels)
+largura = 400
+altura = 500
 
-janela.close()
+#resoluçaõ da tela do usuário
+largura_tela = menu.winfo_screenwidth()
+altura_tela = menu.winfo_screenheight()
+
+#posição da janela (conta realizada para centralizar a aba do app, o valor 50 foi acrescentado devido a aba do pc)
+posicao_x = largura_tela/2 - largura/2
+posicao_y = altura_tela/2 - (altura/2 + 50)
+
+# define as dimensões tela exibida
+menu.geometry("%dx%d+%d+%d" %(largura, altura, posicao_x, posicao_y))
+# define que as dimensões da tela não podem ser alteradas pelo usuário
+menu.resizable(False,False)
+#define a logo de canto da tela, puxando-a do arquivo
+#menu.iconbitmap("imagens/logo.ico")
+#define o fundo menu inicial com a cor vermelho escura
+menu["bg"] = "darkred"
+    
+#define o botão
+alterar_cadastro = Button(menu, text = "Usuário 𓀟",width = 13,justify="left", command = lambda: print("oi"))
+#exibe o botão na tela nas coordenadas expressas
+alterar_cadastro.place(x=0,y=0)
+
+#define o botão
+termo = Button(menu, text = "Termo 🎮",width = 13,justify="center", command = lambda: print("oi"))
+#exibe o botão na tela nas coordenadas expressas
+termo.place(x=97,y=0)
+
+#define o botão
+saida_usuario = Button(menu, text = "Sair ⍈",width = 13,justify="center", command = lambda: print("oi"))
+#exibe o botão na tela nas coordenadas expressas
+saida_usuario.place(x=190,y=0)
+
+#define o botão
+carrinho = Button(menu, text = "Carrinho 🛒",width = 13,justify="center", command = lambda: print("oi"))
+#exibe o botão na tela nas coordenadas expressas
+carrinho.place(x=283,y=0)
+
+scroll_bar = Scrollbar(menu)
+scroll_bar.pack(side = 'right', fill = 'y')
+
+# exibe a aba
+menu.mainloop()
